08a01278b98464a542300684f0bb935ca526f822
Chelsea Wetzel <chelseagilmore14@gmail.com>
2020-01-20 16:53:40 -0500

    knex CRUD

--- posts/post-router.js
+++ posts/post-router.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get('/', async (req, res, next) => {
     try {
-         // translates to SELECT * FROM posts
+         // translates to SELECT * FROM posts;
           res.json(await db.select("*").from("posts"))
     } catch (err) {
         next(err)
@@ -16,7 +16,8 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:id', async (req, res, next) => {
     try {
-
+        // translate to SELECT * FROM posts WHERE id = ?;
+        res.json(await db.select("*").from("posts").where("id", req.params.id))
     } catch (err) {
         next(err)
     }
@@ -24,7 +25,14 @@ router.get('/:id', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
     try {
-        
+        const payload = {
+            title: req.body.title,
+            contents: req.body.contents
+        }
+
+        // translates to INSERT INTO posts (title, contents) VALUES(?, ?);
+        const [id] = await db("posts").insert(payload)
+        res.json(await db("posts").where("id", id).first())
     } catch (err) {
         next(err)
     }
@@ -32,7 +40,14 @@ router.post('/', async (req, res, next) => {
 
 router.put('/:id', async (req, res, next) => {
     try {
-        
+        const payload = {
+            title: req.body.title,
+            contents: req.body.contents
+        }
+
+        // translate to UPDATE posts SET title = ? AND contents = ? WHERE id = ?;
+        await db("posts").where("id", req.params.id).update(payload)
+        res.json(await db("posts").where("id", req.params.id).first())
     } catch (err) {
         next(err)
     }
@@ -40,7 +55,9 @@ router.put('/:id', async (req, res, next) => {
 
 router.delete('/:id', async (req, res, next) => {
     try {
-        
+        // translates to DELETE FROM posts WHERE id = ?;
+        await db("posts").where("id", req.params.id).del()
+        res.status(204).end()
     } catch (err) {
         next(err)
     }
